617f2e1152529a4487bd6da44e8011e21cb9d9b4
Yasmeen <yasmeenmas@gmx.de>
2023-07-17 11:01:04 +0200

    2b. Parametrisierung + Datenübergabe von AUSSEN

--- script_01.js
+++ script_01.js
@@ -16,7 +16,7 @@ function test()
 /***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
-ausgabeNamen();
+// ausgabeNamen();
 
 function ausgabeNamen () {
 // What happens in VEGAS ...
@@ -24,5 +24,17 @@ let firstname = "Yasmeen";
     console.log("Hallo " + firstname + "!");
 }
 
-console.log(firstname); // Fehler --> SCOPE!
+// console.log(firstname); // Fehler --> SCOPE!
 
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// Argumente sind Daten für Parameter
+ausgabeNamen2("Sohrab"); // call +Argument(e)
+ausgabeNamen2("Nico");
+ausgabeNamen2("Yasmeen");
+
+// Funktion mit Parameter(n)
+function ausgabeNamen2 (firstname) {
+    console.log("Hallo " + firstname + "!");
+}
