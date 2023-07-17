fdfedbf9ae8c72465e5673bdec36fc03cc2224dd
Yasmeen <yasmeenmas@gmx.de>
2023-07-17 12:10:27 +0200

    03a. Vorbereitung -Trennen der Funktionalitäten

--- script_01.js
+++ script_01.js
@@ -42,10 +42,27 @@ function ausgabeNamen2 (firstname) {
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter / Argumente
 
-ausgabeNamenParams("Max", "Mütze")
-ausgabeNamenParams(prompt("Vorname?"), prompt("Name?"))
+// ausgabeNamenParams("Max", "Mütze");
+// ausgabeNamenParams(prompt("Vorname?"), prompt("Name?"));
 
 function ausgabeNamenParams(firstName, familyName) {
     // console.log("Hallo " + firstName + " " + familyName + "!"); 
     console.log(`Hallo ${firstName} ${familyName}!`)
+}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenParams2("Max", "Mütze");
+
+function ausgabeNamenParams2(firstName, familyName) {
+    
+    // 1. Funktionalität: string composing
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP+ familyName + "!";
+
+    // 2. Funktionalität: string output
+    console.log(outputStr);
 }
